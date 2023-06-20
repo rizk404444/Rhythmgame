a2901facf2f13fa0b93972ceeeb6e638e4890b43
rizk404444 <65712524+rizk404444@users.noreply.github.com>
2023-06-20 16:19:49 +0900

    0.0.8
    ver0.0.8

---
+++ dynamic_beat_8/Track.java
@@ -0,0 +1,52 @@
+package dynamic_beat_8;
+
+public class Track {
+
+	private String titleImage; //제목이미지
+	private String startImage; //게임 선택 창 표 이미지
+	private String gameImage; //해당곡을 실행했을때 표지
+	private String startMusic; //게임 선택창 음악
+	private String gameMusic; //해당곡을 실행했을때
+	public String getTitleImage() {
+		return titleImage;
+	}
+	public void setTitleImage(String titleImage) {
+		this.titleImage = titleImage;
+	}
+	public String getStartImage() {
+		return startImage;
+	}
+	public void setStartImage(String startImage) {
+		this.startImage = startImage;
+	}
+	public String getGameImage() {
+		return gameImage;
+	}
+	public void setGameImage(String gameImage) {
+		this.gameImage = gameImage;
+	}
+	public String getStartMusic() {
+		return startMusic;
+	}
+	public void setStartMusic(String startMusic) {
+		this.startMusic = startMusic;
+	}
+	public String getGameMusic() {
+		return gameMusic;
+	}
+	public void setGameMusic(String gameMusic) {
+		this.gameMusic = gameMusic;
+	}
+	
+	public Track(String titleImage, String startImage, String gameImage, String startMusic, String gameMusic) {
+		super();
+		this.titleImage = titleImage;
+		this.startImage = startImage;
+		this.gameImage = gameImage;
+		this.startMusic = startMusic;
+		this.gameMusic = gameMusic;
+	}
+	
+	
+	
+}
